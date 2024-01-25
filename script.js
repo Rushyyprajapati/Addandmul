1fbf8e338aa3a0d55ede987bb2872a0d10dd7d22
Rushyyprajapati <145515777+Rushyyprajapati@users.noreply.github.com>
2024-01-25 14:08:22 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+function performAddition() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 + num2;
+    document.getElementById('result').innerText = `Result: ${result}`;
+}
+
+function performMultiplication() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 * num2;
+    document.getElementById('result').innerText = `Result: ${result}`;
+}
